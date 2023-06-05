9df364bad547d4e0a52d83c95d08a4147ac08b83
nastyashev <nastya.sova2004@gmail.com>
2023-06-06 01:51:05 +0300

    lab5

---
+++ lab5/Mediator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5
+{
+    // Інтерфейс посередника
+    interface IMediator
+    {
+        void RegisterAircraft(Aircraft aircraft);
+        void Land(Aircraft aircraft);
+        void Takeoff(Aircraft aircraft);
+    }
+
+    // Конкретний посередник
+    class ConcreteMediator : IMediator
+    {
+        private List<Aircraft> aircraftList = new List<Aircraft>();
+        private List<bool> runways = new List<bool>();
+        private List<bool> terminals = new List<bool>();
+
+        public ConcreteMediator()
+        {
+            // Ініціалізуємо полоси та термінали як вільні (true)
+            for (int i = 0; i < 4; i++)
+            {
+                runways.Add(true);
+                terminals.Add(true);
+            }
+        }
+
+        public void RegisterAircraft(Aircraft aircraft)
+        {
+            aircraftList.Add(aircraft);
+        }
+
+        public void Land(Aircraft aircraft)
+        {
+            int runwayIndex = GetFreeRunwayIndex();
+            if (runwayIndex != -1)
+            {
+                runways[runwayIndex] = false; // Займаємо полосу
+                Console.WriteLine($"Aircraft {aircraft} is landing on Runway {runwayIndex + 1}.");
+                // Виконати інші дії для посадки літака
+            }
+            else
+            {
+                Console.WriteLine($"No available runway for landing the aircraft {aircraft}.");
+                // Виконати інші дії, які стосуються відсутності доступних полос для посадки
+            }
+        }
+
+        public void Takeoff(Aircraft aircraft)
+        {
+            int runwayIndex = GetFreeRunwayIndex();
+            if (runwayIndex != -1)
+            {
+                runways[runwayIndex] = false; // Займаємо полосу
+                Console.WriteLine($"Aircraft {aircraft} is taking off from Runway {runwayIndex + 1}.");
+                // Виконати інші дії для взліту літака
+            }
+            else
+            {
+                Console.WriteLine($"No available runway for takeoff the aircraft {aircraft}.");
+                // Виконати інші дії, які стосуються відсутності доступних полос для взліту
+            }
+        }
+
+        private int GetFreeRunwayIndex()
+        {
+            for (int i = 0; i < runways.Count; i++)
+            {
+                if (runways[i])
+                {
+                    return i; // Повертаємо індекс доступної полоси
+                }
+            }
+            return -1; // Повертаємо -1, якщо всі полоси зайняті
+        }
+    }
+}
---
+++ lab5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Реалізувати задачу контролю руху літаків диспетчерами. Самі літакі
+напряму не взаємодіють один з одним. Повинно бути мінімум 4 взлітнопосадкових полос 
+та 4 термінали. Одночасно на кожній полосі та у
+кожного терміналу може знаходитись тільки один літак.
+ */
+
+namespace lab5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcreteMediator mediator = new ConcreteMediator();
+
+            while (true)
+            {
+                Console.WriteLine("Enter 'L' for landing request or 'T' for takeoff request (press 'Q' to quit):");
+                string input = Console.ReadLine();
+
+                if (input.ToUpper() == "L")
+                {
+                    ConcreteAircraft aircraft = new ConcreteAircraft(mediator);
+                    mediator.RegisterAircraft(aircraft);
+                    aircraft.RequestLanding();
+                }
+                else if (input.ToUpper() == "T")
+                {
+                    ConcreteAircraft aircraft = new ConcreteAircraft(mediator);
+                    mediator.RegisterAircraft(aircraft);
+                    aircraft.RequestTakeoff();
+                }
+                else if (input.ToUpper() == "Q")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
